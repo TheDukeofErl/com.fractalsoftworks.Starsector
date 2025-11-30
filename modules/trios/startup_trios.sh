3300b6ac42491b083bc2f6c0f2495dbf40302155
Kevin <TheDukeofErl@users.noreply.github.com>
2025-11-29 20:10:28 -0600

    Fix launching starsector from trios on the first launch

--- modules/trios/startup_trios.sh
+++ modules/trios/startup_trios.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-mkdir -p "${XDG_DATA_HOME}/saves" "${XDG_DATA_HOME}/mods" "${XDG_DATA_HOME}/screenshots" "${XDG_DATA_HOME}/logs" "${XDG_DATA_HOME}/java"
+mkdir -p "${XDG_DATA_HOME}/saves" "${XDG_DATA_HOME}/mods" "${XDG_DATA_HOME}/screenshots" "${XDG_DATA_HOME}/logs" "${XDG_DATA_HOME}/java"  "${XDG_DATA_HOME}/config"
 ln -s "${XDG_DATA_HOME}/java" ~/.java
+# if the settings file from the game is newer, we gotta clobber what we've got for what we don't got
+cp -u /app/starsector/data/config/original_settings.json "${XDG_DATA_HOME}/config/settings.json"
 
 if [ -z "$XDG_DATA_HOME" ]; then
     XDG_DATA_HOME="$HOME/.local/share"
